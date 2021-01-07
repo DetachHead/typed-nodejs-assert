ff7b113852edef47d50c0ca71e8cfb712a9c6d6e
DetachHead <detachhead@users.noreply.github.com>
2021-01-07 21:38:29 +1000

    fix assertion type issue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import assert = require('assert')
+import _assert = require('assert')
 
-type Assert = Omit<typeof assert, "deepStrictEqual" | "strictEqual"> & {
+type Assert = Omit<typeof _assert, "deepStrictEqual" | "strictEqual"> & {
     (value: any, message?: string | Error): asserts value; //TODO: figure out why typeof assert removes this call sig
     strictEqual<T extends U, U = T>(actual: U, expected: T, message?: string): asserts actual is T;
     deepStrictEqual<T extends U, U = T>(actual: U, expected: T, message?: string): asserts actual is T;
 };
 
-//TODO: remove the compiled JS, we are only fixing types here
-export = assert as Assert
+//hack https://github.com/microsoft/TypeScript/issues/34596#issuecomment-691574987
+const assert: Assert = _assert
+
+export = assert
